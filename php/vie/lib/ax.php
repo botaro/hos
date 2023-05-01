9ad3bfbc4d9b745796dfae8be31e1c4f5999f6fd
botaro <binbotaro3@gmail.com>
2023-05-01 16:27:08 +0900

    2023-05-01-16-27-08

--- php/vie/lib/ax.php
+++
@@ -1,5 +0,0 @@
-<script src=https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js>
-</script>
-
-<script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous"></script>
-
