b81e0d92bbdc56431d510dd129ce2d32d7fccc19
botaro <binbotaro3@gmail.com>
2023-05-01 18:56:17 +0900

    2023-05-01-18-56-17

--- php/def.php
+++ php/def.php
@@ -1,4 +1,11 @@
 <?php
 define("rt",dirname(__FILE__));
+$pos="/mnt/home/.config/composer/vendor/autoload.php";
+
+$son=file_get_contents("./.env.json");
+$ar=json_decode($son);
+
+define("nam",$ar->nam);
+define("pss",$ar->pss);
 
 ?>
